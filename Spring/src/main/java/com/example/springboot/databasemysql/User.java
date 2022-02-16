a944e0256e6e56d1fce2b3d8ea8acd615104f8a3
Atul Sharma <2012atulsharma@gmail.com>
2022-02-16 18:47:55 +0530

    Initial commit

---
+++ Spring/src/main/java/com/example/springboot/databasemysql/User.java
@@ -0,0 +1,39 @@
+package com.example.springboot.databasemysql;
+
+import javax.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "user_table")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ Spring/src/test/java/com/example/springboot/ApplicationTests.java
@@ -0,0 +1,43 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApplicationTests {
+
+    @Autowired
+    private HelloController helloController;
+
+    @Autowired
+    private MockMvc mvc;
+
+    // Uncomment this test files after adding connection-end points in application.properties to run the tests
+   /*
+   @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+
+
+    @Test
+    void version() {
+        assertEquals( "The actual version is 1.0.0", helloController.version());
+    }
+
+    */
+
+}
